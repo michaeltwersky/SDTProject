09165cac3dd8a7c4c96f06d7632d438656257b7e
michaeltwersky <148817306+michaeltwersky@users.noreply.github.com>
2023-11-06 12:04:52 -0500

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -2,38 +2,55 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+# Loading the dataset
 cars = pd.read_csv('vehicles_us.csv')
 
+# Title of the Web App
 st.title('Vehicle Data Analysis')
 
+# Project Description and Libraries Used
 st.markdown("""
 This web app analyzes and plots vehicle data
 * **Python libraries:** pandas, streamlit, base64, plotly.express
 * **Data source:** CSV File
 """)
 
+# Adding a column that shows just the manufacturer 
 cars['manufacturer'] = cars['model'].apply(lambda x: x.split()[0])
 
+# Displaying the dataframe
 st.header('Data Viewer')
 st.dataframe(cars)
 
 
+# Finding duplicated and missing values in the dataframe
 
 cars.duplicated().sum()
 
 cars.isnull().sum()
 
+# Filling missing model year values with unknown
 cars['model_year'] = cars['model_year'].fillna(value='unknown')
-cars['cylinders'] = cars['cylinders'].fillna(value=cars['cylinders'].mean())
-# I wasn't sure what to replace the unknown odometer values with (median, mean, or unknown).
-cars['odometer'] = cars['odometer'].fillna(value='unknown')
-cars['paint_color'] = cars['paint_color'].fillna(value='unknown')
-cars['is_4wd'] = cars['is_4wd'].fillna(value='unknown')
 
+# Filling missing number of cylinders with the mean. This is because there were no outliers in the limited range of possibilities of number of cylinders.
+cars['cylinders'] = cars['cylinders'].fillna(value=cars['cylinders'].mean())
+
+# I wasn't sure what to replace the unknown odometer values with (median, mean, or unknown).
+
+# Filling missing odometer color values with unknown as the range in possibile values is very large and likely contains outliers
+cars['odometer'] = cars['odometer'].fillna(value='unknown')
+
+# Filling missing paint color values with unknown
+cars['paint_color'] = cars['paint_color'].fillna(value='unknown')
+
+# Filling missing values in this column with - to keep the column as type bool.
+cars['is_4wd'] = cars['is_4wd'].fillna(value=0)
+
+# Plotting a scatterplot with relationship between model year and vehicle price
 fig = px.scatter(cars, x='model_year', y='price', title='Vehicle Model Year to Price')
 st.plotly_chart(fig)
 
-
+# Plotting an interactive bar chart with the model year and conditions of the car
 fig2 = px.bar(cars, x='model_year', y='condition', color='condition', title='Vehicle Model Year to Condition')
 st.plotly_chart(fig2)
 
